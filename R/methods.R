c3e1ac9a1cd75d6d28e9eb374bffe34257d0ab29
Tuomas Rajala <tuomas.rajala@iki.fi>
2016-01-19 20:55:09 +0100

    Remove dependency on rgltools
    * 3D pcf plot now defunct, it depended on a private package rgltools

--- R/methods.R
+++ R/methods.R
@@ -9,7 +9,7 @@ plot.os_K <- function(x, ...){
 
 #' method plot for anisotropic pcf-function
 #' 
-#' @import sphere rgltools
+#' @import sphere
 #' @export
 #' @exportMethod plot
 
@@ -18,18 +18,19 @@ plot.pcf_anisotropic <- function(x, ab, ...){
   # 2d:
   if(x$dim==2) image(z=x$est, x=x$r, y=x$theta[[1]], xlab="range", ylab="angle", ...)
   else{
+    stop("Plot not implemented for 3D.")
     # more tricky. We will plot a integral over the range:
-    lon <- azi2lon(x$theta[[1]])
-    lat <- inc2lat(x$theta[[2]])
-    
-    if(missing(ab)) ab <- range(x$r)
-    ok <- x$r <= ab[2] & x$r >= ab[1]
-    v <- apply(x$est[ok,,], 3, apply, 2, sum) - 1
-    ll <- ai2ll(expand.grid(lat, lon))
-    #' add antipode
-    ll <- rbind(ll, antipode(ll))
-    s <- rgltools.smoothsphere(ll, c(c(v), c(v)))
-    rgltools.plotsmoothsphere(s, ...)
+#     lon <- azi2lon(x$theta[[1]])
+#     lat <- inc2lat(x$theta[[2]])
+#     
+#     if(missing(ab)) ab <- range(x$r)
+#     ok <- x$r <= ab[2] & x$r >= ab[1]
+#     v <- apply(x$est[ok,,], 3, apply, 2, sum) - 1
+#     ll <- ai2ll(expand.grid(lat, lon))
+#     #' add antipode
+#     ll <- rbind(ll, antipode(ll))
+#     s <- rgltools.smoothsphere(ll, c(c(v), c(v)))
+#     rgltools.plotsmoothsphere(s, ...)
     
   }
   invisible(s)
@@ -37,7 +38,7 @@ plot.pcf_anisotropic <- function(x, ab, ...){
 
 #' method plot2d for anisotropic pcf-function
 #' 
-#' @import sphere rgltools
+#' @import sphere
 #' @export
 #' @exportMethod plot2d
 
