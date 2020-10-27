54d8d261ae6316813e0785d6526721333fbcdf47
Tuomas R <tuomas.rajala@iki.fi>
2020-10-27 12:28:47 +0200

    rudimentary wavelet transform of intensity in the form of scale-angle-energy-density

---
+++ R/wavelet-scale-angle-energy-density.R
@@ -0,0 +1,55 @@
+#' Scale-angle-energy-density
+#' 
+#' 2D at the moment. Morlet anisotropic wavelet
+#' 
+#' @param x point pattern, list of $x and $bbox
+#' @param theta angles
+#' @param shift_res resolution of the shift-grid
+#' @param ... ignored
+
+wavelet_saed <- function(x, theta = seq(0, pi, l = 30), 
+                            shift_res = 20,
+                            scales) {
+  x <- check_pp(x)
+  loc <- t( x$x )
+  bb <- x$bbox
+  sl <- apply(bb, 2, diff)
+  
+  if(length(shift_res)<2) shift_res <- shift_res * c(1, sl[2]/sl[1])
+  if(missing(scales)) scales <- seq(0, max(sl), length.out = 20)
+  # wavelet
+  # mother wavelet, fixed
+  D <- 0.1
+  k0 <- cbind(0, 5.5)
+  con <- sqrt(D/pi) 
+  
+  wave <- function(a, b, ang) {
+    R <- rotationMatrix3(az = -ang)[-3,-3]
+    zx <- R%*%(loc - b) /a  
+    #e <- exp(1i * k0 %*% zx - 0.5 * colSums((zx)^2) )
+    #psia2 <- abs(e * con)^2
+    d <- k0%*%zx
+    psia2 <- con^2 * exp(-(zx[1,]^2*D^2 + zx[2,]^2)) * ( sin(d)^2 + cos(d)^2 )
+    sum(psia2)/a
+  }
+  ## 
+  bgrid <- as.matrix(expand.grid( bx <-  seq(bb[1,1], bb[2,1], l = shift_res[1]),
+                        by <-  seq(bb[1,2], bb[2,2], l = shift_res[2])) )
+  db <- diff(bx[1:2])*diff(by[1:2])
+  S <- sapply( theta, function(ang) {
+    R <- rotationMatrix3(az = ang)[-3,-3]
+    v <- sapply(scales, function(a){
+      sa <- apply(bgrid, 1, function(b){
+        zx <- R%*%(loc - b) /a  
+        d <- k0 %*% zx
+        psia2 <- con^2 * exp(-(zx[1,]^2*D^2 + zx[2,]^2)) * ( sin(d)^2 + cos(d)^2 )
+        sum(psia2)/a
+      })
+      sum(sa^2) * db
+    })
+    v
+  })
+  S
+}
+
+
---
+++ tests/69-wavelet-energy.R
@@ -0,0 +1,21 @@
+# dev and test wavelet transform of intensity
+
+library(devtools)
+load_all(".")
+#source("tests/test-data.R")
+
+#x <- pp
+source("~/Sync/Aka/julkaisut/anisotropy-review-overleaf/examples/synthetic-examples/generate-stripe.R")
+x <- stripes
+
+#nn <- 100
+#x <-  cbind(runif(nn) -.5, runif(nn) - .5)  * 60
+#x[,2] <- round(x[,2])
+
+t0 <- system.time(S <- wavelet_saed(x, 
+                  scales = scales <- seq(0, 1, l = 10)[-1], 
+                  theta =  theta  <- seq(0, pi, l = 10)[-1], shift_res = 10) )
+print(t0)
+
+image(y = scales, x = theta, matrix(S, byrow=T, ncol = length(scales)))#, x=theta, y=scales)
+
