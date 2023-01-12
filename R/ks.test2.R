44970727a323e7dc0f2765af03dbe2ab5456062a
Tuomas Rajala <tuomas.rajala@iki.fi>
2023-01-12 09:15:46 +0200

    remove ks.test2

--- R/ks.test2.R
+++ R/ks.test2.R
@@ -1,16 +1,17 @@
-#' Kolmogorov-Smirnov test using ecdf's
-#' 
-#' @param x empirical cdf function, see details
-#' @param y theoretical cdf function
-#' @param n Number of 'datapoints'
-#' 
-#' @useDynLib Kdirectional
-#' @export
-
-ks.test2 <- function(x, y, n) {
-  if(ncol(x)!=2)stop("x should be 2-col matrix with first col the domain and second column the cdf values.")
-  if(missing(n))stop("Need the number of datapoints from which x is computed.")
-  d <- max(  abs(x[,2]-y(x[,1])) )
-  p <- c_p_of_KStest(n, d)
-  c(statistic=d, p.value=p)
-}
+# #' Kolmogorov-Smirnov test using ecdf's
+# #' 
+# #' @param x empirical cdf function, see details
+# #' @param y theoretical cdf function
+# #' @param n Number of 'datapoints'
+# #' 
+# #' @useDynLib Kdirectional
+# #' @export
+# 
+# ks.test2 <- function(x, y, n) {
+#   if(ncol(x)!=2)stop("x should be 2-col matrix with first col the domain and second column the cdf values.")
+#   if(missing(n))stop("Need the number of datapoints from which x is computed.")
+#   d <- max(  abs(x[,2]-y(x[,1])) )
+#   p <- c_p_of_KStest(n, d)
+#   c(statistic=d, p.value=p)
+# }
+## Obsolete.
